4c2dfb7420b1055b7ce67a04391692e3182f69bd
sungmino <44549382+sungmino@users.noreply.github.com>
2020-05-29 10:04:46 +0700

    Configure entity with Fluent API

---
+++ smnShopSolution.Data/Configurations/TransactionConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Transactions;
+
+namespace smnShopSolution.Data.Configurations
+{
+    public class TransactionConfiguration : IEntityTypeConfiguration<Transaction>
+    {
+        public void Configure(EntityTypeBuilder<Transaction> builder)
+        {
+            builder.ToTable("Transactions");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id).UseIdentityColumn();
+        }
+    }
+}
--- smnShopSolution.Data/EF/smnShopDbContext.cs
+++ smnShopSolution.Data/EF/smnShopDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using smnShopSolution.Data.Entities;
 using System;
 using System.Collections.Generic;
 using System.Text;
