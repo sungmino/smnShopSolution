ee2a1c90b08acd7dfab81508740d8762f678817c
sungmino <44549382+sungmino@users.noreply.github.com>
2020-06-04 12:36:36 +0700

    delete view folder

---
+++ smnShopSolution.RestFullApi/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace smnShopSolution.RestFullApi.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
